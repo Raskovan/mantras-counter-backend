d6741aa46ddfcc0bdc2850352b586aa67f7704df
github username <github email address>
2020-02-17 11:08:05 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const nconf = require('nconf')
+const bodyParser = require('body-parser')
+
+nconf
+  .argv()
+  .env('__')
+  .file({ file: './config.json' })
+
+app.use(bodyParser.json())
+
+app.get('/_health', function(req, res) {
+  res.json('Karmapachenno')
+})
+
+app.use('/api/v1', require('./lib/routes'))
+
+app.use((req, res, next) => {
+  console.info(`${new Date().toString()} => ${req.originalUrl}`, req.body)
+  res.status(404).send('STOP! You are hitting the wrong endpoint. Try again!')
+})
+
+let listener = app.listen(process.env.PORT || 8080, () =>
+  console.info('Server has started on port ' + listener.address().port)
+)
